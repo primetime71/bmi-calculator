779b7cca63c0086f226ed643b01360272d0e7609
primetime71 <shantanu3543@gmail.com>
2021-11-12 00:49:39 +0530

    added print area and js for printing

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ function calcBMI(){
     let height=document.getElementById('height').value;
     const convHeight=(height/100)*(height/100);
     let bmi=weight/convHeight;
-   let finalBMI=bmi.toFixed(2);
-
-    return alert(finalBMI);
-    
-
+    let finalBMI=bmi.toFixed(2);
+        return setBMI(finalBMI);
 }
+
+function setBMI(result){
+    const ans=`Your BMI is: ` + result;
+    const opText=document.getElementById("result").innerText=ans;
+}
+
 
 
 
